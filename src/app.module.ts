6432d4a36c0e08c919e26cf6410136fef0555483
Yury Popeko <yurypopeko@gmail.com>
2020-03-28 22:31:09 +0300

    HQOSG-25: customize environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { SyncModule } from './sync/sync.module';
+
+const environment = process.env.NODE_ENV || 'development';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SyncModule,
+
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_WRITE_CONNECTION_STRING,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    ),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
