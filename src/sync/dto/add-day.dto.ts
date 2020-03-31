563276152d6abe9745d92e08ccedac24bc9bb683
Yury Popeko <yurypopeko@gmail.com>
2020-03-31 12:26:37 +0300

    HQOSG-27: controller & swagger

--- src/sync/dto/add-day.dto.ts
+++ src/sync/dto/add-day.dto.ts
@@ -1,5 +1,23 @@
+import { IsInt, IsString, IsNotEmpty, Min, Max } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+const SECONDS_IN_DAY = 60 * 60 * 24;
+
 export class AddDayDto {
+  @IsInt()
+  @Min(0)
+  @Max(SECONDS_IN_DAY)
+  @ApiProperty()
   readonly usefull: number;
+
+  @IsInt()
+  @Min(0)
+  @Max(SECONDS_IN_DAY)
+  @ApiProperty()
   readonly wasted: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
   readonly date: string;
 }
--- src/sync/sync.controller.ts
+++ src/sync/sync.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { SyncService } from './sync.service';
+import { AddDayDto } from './dto/add-day.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { ISync } from './interfaces/sync.interface';
 
+@ApiTags('sync')
 @Controller('sync')
-export class SyncController {}
+export class SyncController {
+  constructor(private readonly syncService: SyncService) { }
+
+  @Post()
+  async add(@Body(ValidationPipe) day: AddDayDto): Promise<ISync> {
+    return this.syncService.add(day)
+  }
+}
